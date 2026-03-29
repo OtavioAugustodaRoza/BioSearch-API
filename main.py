676b2fa21cc63642e13fe3a08808906f4e46ebf0
Otávio <otavioaugustodaroza@gmail.com>
2026-03-29 18:35:03 -0300

    Restrict CORS to specific origin

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["https://bio-search-front.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
